69c634bbe3bf6861e1292d1ca1e67d05d2472b9d
itc-n26014 <n26014@std.it-college.ac.jp>
2026-07-09 02:49:02 +0000

    whileを使ったプログラミング

--- chap03/q3_6_1.py
+++ chap03/q3_6_1.py
@@ -1,7 +1,9 @@
 import random
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+numbers = [chr(i) for i in range(ord('0'), ord('9') +1)]
+#print(numbers)
 sample4 = random.sample(numbers, k=4)
 num4 = ''.join(sample4)
+print(num4)
 while True:
     val = input()
     if val == num4:
