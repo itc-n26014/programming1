745b86030ce15749cabab81e9afbb228c8f06d45
itc-n26014 <n26014@std.it-college.ac.jp>
2026-07-21 02:32:53 +0000

    リストのメソッド

---
+++ chap05/q5_1_3.py
@@ -0,0 +1,8 @@
+queue = []
+queue.append(1)
+print('queue:', queue)
+queue.append(2)
+print('queue:', queue)
+print('pop from queu 1st value:', queue.pop(0))
+print('pop from queu 2nd value:', queue.pop(0))
+print('queue:', queue)
